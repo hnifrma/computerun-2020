0d435f5fd708fcf7f1e5b636cdeb6600244a6de7
reinhart1010 <reinhart_previano@yahoo.com>
2020-09-16 17:17:16 +0700

    feat(frontend): Add footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="himsisfo-himti-container">
+        <img src="/img/icons/HIMSISFO Logo.png">
+        <img src="/img/icons/HIMTI Logo.png">
+    </div>
+    <div class="copyright">
+        HIMSISFO & HIMTI BINUS University<br>
+        &copy; 2020
+    </div>
+</footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -100,15 +100,7 @@
             </div>
             <img class="container-clip for-footer" src="/img/backgrounds/7.png">
         </div>
-        <footer class="footer">
-            <div class="himsisfo-himti-container">
-                <img src="/img/icons/HIMSISFO Logo.png">
-                <img src="/img/icons/HIMTI Logo.png">
-            </div>
-            <div class="copyright">
-                HIMSISFO & HIMTI BINUS University<br>
-                &copy; 2020
-            </div>
-        </footer>
+        @component('components.footer')
+        @endcomponent
     </body>
 </html>
