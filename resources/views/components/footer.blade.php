85acd2e013e340dff46fc1964fe8501ccd24e40e
reinhart1010 <reinhart_previano@yahoo.com>
2020-09-17 10:02:32 +0700

    feat(frontend): Add footer logo

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,6 +4,7 @@
         <img src="/img/icons/HIMTI Logo.png">
     </div>
     <div class="copyright">
+        <img src="/img/icons/Computerun 2020 Insight Logo.png"><br>
         HIMSISFO & HIMTI BINUS University<br>
         &copy; 2020
     </div>
