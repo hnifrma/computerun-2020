0df9b122887254823c9e330162ce193d74077949
reinhart1010 <reinhart_previano@yahoo.com>
2020-10-27 14:04:37 +0700

    feat: Added new sponsors & icon sizes

--- resources/views/components/sponsors.blade.php
+++ resources/views/components/sponsors.blade.php
@@ -1,8 +1,12 @@
 <h1 class="full-underline content-divider">our partners</h1>
 <h3 class="text-center">SPONSORS</h3>
 <div class="content-divider-short text-center margin-1">
-    <div class="sponsors-container">
-        @component ('components.sponsor-item', ['id' => 'indo-bangunan', 'name' => 'Indo Bangunan'])
+    <div class="sponsors-container large">
+        @component ('components.sponsor-item', ['id' => 'acc-astra', 'name' => 'ACC Astra', 'square' => true])
+        @endcomponent
+    </div>
+    <div class="sponsors-container small">
+        @component ('components.sponsor-item', ['id' => 'indo-bangunan', 'name' => 'Indo Bangunan', 'square' => true])
         @endcomponent
         @component ('components.sponsor-item', ['id' => 'sandimas', 'name' => 'Sandimas Granite & Sanitary'])
         @endcomponent
@@ -12,28 +16,28 @@
 
 <h3 class="text-center">MEDIA PARTNERS</h3>
 <div class="content-divider-short text-center margin-1">
-    <div class="sponsors-container">
+    <div class="sponsors-container small">
         @component ('components.sponsor-item', ['id' => 'binus-tv-club', 'name' => 'BINUS TV Club'])
         @endcomponent
         @component ('components.sponsor-item', ['id' => 'bvoice-radio', 'name' => 'BVoice Radio'])
         @endcomponent
         @component ('components.sponsor-item', ['id' => 'cyber-security-community-binus', 'name' => 'Cyber Security Community, BINUS University'])
         @endcomponent
+        @component ('components.sponsor-item', ['id' => 'dpm-fti-untar', 'name' => 'DPM FTI UNTAR'])
+        @endcomponent
         @component ('components.sponsor-item', ['id' => 'filemagz', 'name' => 'FILE Magazine'])
         @endcomponent
-        @component ('components.sponsor-item', ['id' => 'infolomba-mahasiswa', 'name' => 'Info Lomba Mahasiswa'])
+        @component ('components.sponsor-item', ['id' => 'infolomba-mahasiswa', 'name' => 'Info Lomba Mahasiswa', 'square' => true])
         @endcomponent
         @component ('components.sponsor-item', ['id' => 'kabarkampus.com', 'name' => 'KabarKampus.com', 'baseline' => true])
         @endcomponent
         @component ('components.sponsor-item', ['id' => 'kampus-media-indonesia', 'name' => 'Kampus Media Indonesia'])
         @endcomponent
-        @component ('components.sponsor-item', ['id' => 'kawankampus', 'name' => 'kawankampus'])
+        @component ('components.sponsor-item', ['id' => 'kawankampus', 'name' => 'kawankampus', 'square' => true])
         @endcomponent
         @component ('components.sponsor-item', ['id' => 'radio-kampus-itb', 'name' => 'Radio Kampus ITB', 'baseline' => true])
         @endcomponent
         @component ('components.sponsor-item', ['id' => 'radio-universitas-tarumanagara', 'name' => 'Radio Universitas Tarumanagara'])
-        @endcomponent
-        @component ('components.sponsor-item', ['id' => 'dpm-fti-untar', 'name' => 'DPM FTI UNTAR'])
         @endcomponent
     </div>
     <p>Do you want to sponsor our event too? <b><a href="/sponsor-us">Run before it's too late!</a></b></p>
